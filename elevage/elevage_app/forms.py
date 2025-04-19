b321de83c344b63053c3116a3bde73e4ad940138
CHEN Zhijiang <Zhijiang.CHEN@student.isae-supaero.fr>
2025-04-19 17:55:26 +0800

    create the formule of forms which call ActionForm

--- elevage/elevage_app/forms.py
+++ elevage/elevage_app/forms.py
@@ -4,5 +4,12 @@ class ElevageForm(forms.ModelForm):
     class Meta:
         model=Elevage
         fields='__all__'
+        
+        
+class ActionForm(forms.Form):
+    vendre=forms.IntegerField(min_value=0,label="Lapins a vendre")
+    acheter_nourriture=forms.FloatField(min_value=0,label="Nourriture a acheter_kg")
+    acheter_cages=forms.IntegerField(min_value=0,label="Cages a acheter")
+    
     
     
